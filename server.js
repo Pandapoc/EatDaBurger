967c4a6b7b2ff9eeb026cc734f3f003ba62a5e1c
Pandapoc <45634681+Pandapoc@users.noreply.github.com>
2019-05-18 14:02:10 -0700

    server init

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
+const express = require('express')
+const app = express()
+const exphs = require('express-handlebars')
+const { join } = require('path')
+
+app.use(express.static(join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.engine('.hbs', require('express-handlebars')({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', '.hbs')
+
+app.listen(3000)
+
 console.log('ping')
